873c62468831c40f02c9609e8490d555159819f8
DeedocMisha <mishasvitski@gmail.com>
2024-12-12 21:47:31 +0300

    first commit

---
+++ EmptyVue/src/components/MaIne.vue
@@ -0,0 +1,16 @@
+<template >
+  <div>
+<h>Hello From Sass!!!</h>
+<div class="procent">
+  <a >Procent from a</a>
+</div>
+    <div id="Join">
+    <button class="first">Text1</button>
+      <button class="second">Text2</button>
+    </div>
+  </div>
+  <button class="iff">If / Else</button>
+  <button class="func">Function</button>/
+</template>
+<script setup lang="ts">
+</script>
